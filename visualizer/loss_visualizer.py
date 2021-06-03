f55268bec60e5fd4762413494906bdc85c4ce4ac
Ali <alimirzazadeham@gmail.com>
2021-06-03 04:20:23 +0200

    loss visualizeation

--- visualizer/loss_visualizer.py
+++ visualizer/loss_visualizer.py
@@ -1,20 +1,62 @@
 # import pandas as pd
-import torch
+# import torch
 import matplotlib.pyplot as plt
 import numpy as np
 def visualizeLosses():
-	# pdFile = pd.read_csv('saved_figs/track_lossImg.csv')
-	data = np.load('saved_figs/track_lossImg.npy')
-	data = 255 * data
-	data = data.astype(int)
-	data2 = np.load('saved_figs/track_lossNum.npy')
-	print(np.max(data), np.min(data))
-	print(data.shape)
-	print(data2[1,:])
-	plt.imshow(data[1,:,:])
-	plt.savefig('test.png')
+    # pdFile = pd.read_csv('saved_figs/track_lossImg.csv')
+    data = np.load('track_lossImg.npy')
+    
+    data -= np.min(data)
+    # print(np.max(data), np.min(data), np.mean(data))
 
-	# print(pdFile.iloc[0,1])
-	# plt.show()
-	# print(pdFile)
-visualizeLosses()
+    # cont = np.greater(data, np.mean(data) + 2*np.std(data))
+    # data[cont] = np.mean(data) + 2*np.std(data)
+    # data = 255 * data
+    data = data.astype(int)
+    # thisData = data[2,:,:]
+    data2 = np.load('track_lossNum.npy')
+    # print(np.max(thisData), np.min(thisData), np.mean(thisData))
+    # print(thisData.shape)
+    # plt.imshow(thisData)
+    # plt.show()
+
+
+    class IndexTracker:
+        def __init__(self, ax, X):
+            self.ax = ax
+            ax.set_title('use scroll wheel to navigate images')
+
+            self.X = X
+            self.slices, rows, cols = X.shape
+            self.ind = self.slices//2
+
+            self.im = ax.imshow(self.X[self.ind,:, :])
+            self.update()
+
+        def on_scroll(self, event):
+            # print("%s" % (event.key))
+            # print(self.slices)
+            if event.key == 'j':
+                if (self.ind + 1) < self.slices:
+                    self.ind = (self.ind + 1) % self.slices
+            elif event.key == 'k':
+                if (self.ind - 1) >= 0:
+                    self.ind = (self.ind - 1) % self.slices
+            self.update()
+
+        def update(self):
+            self.im.set_data(self.X[self.ind,:, :])
+            self.ax.set_ylabel('slice %s' % self.ind)
+            self.im.axes.figure.canvas.draw()
+
+
+    fig, ax = plt.subplots(1, 1)
+
+    tracker = IndexTracker(ax, data)
+
+    fig.canvas.mpl_connect('key_press_event', tracker.on_scroll)
+    plt.show()
+    # print(pdFile.iloc[0,1])
+    # plt.show()
+    # print(pdFile)
+# visualizeLosses()

bb0f22e467ce3f5914050cc58334be7aad50acad
krinetic1234 <56781484+krinetic1234@users.noreply.github.com>
2021-07-14 22:53:57 -0700

    Update smooth_grad.py

--- libs/pytorch_grad_cam/smooth_grad.py
+++ libs/pytorch_grad_cam/smooth_grad.py
@@ -6,6 +6,37 @@ from torch.autograd import Variable
 from torch.nn import functional as F
 
 
+class VanillaGrad(object):
+
+    def __init__(self, pretrained_model, cuda=False):
+        self.pretrained_model = pretrained_model
+        self.features = pretrained_model.features
+        self.cuda = cuda
+        # self.pretrained_model.eval()
+
+    def __call__(self, x, index=None):
+        output = self.pretrained_model(x)
+
+        if index is None:
+            index = np.argmax(output)
+
+        one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
+        one_hot[0][index] = 1
+        # if self.cuda:
+        #     one_hot = Variable(torch.from_numpy(
+        #         one_hot).cuda(), requires_grad=True)
+        # else:
+        # one_hot = Variable(torch.from_numpy(one_hot), requires_grad=True)
+        one_hot = torch.sum(one_hot * output)
+
+        torch.autograd.grad(one_hot, output, create_graph=True)
+
+        grad = x.grad
+        grad = grad[0, :, :, :]
+
+        return grad
+
+
 class SmoothGrad(VanillaGrad):
 
     def __init__(self, pretrained_model, cuda=False, stdev_spread=0.15,
@@ -22,39 +53,39 @@ class SmoothGrad(VanillaGrad):
         self.magnitude = magnitude
 
     def __call__(self, x, index=None):
-        x = x.data.cpu().numpy()
         stdev = self.stdev_spread * (torch.max(x) - torch.min(x))
         total_gradients = torch.zeros_like(x)
         for i in range(self.n_samples):
             noise = torch.random.normal(
                 0, stdev, x.shape).astype(torch.float32)
             x_plus_noise = x + noise
-            if self.cuda:
-                x_plus_noise = Variable(torch.from_numpy(
-                    x_plus_noise).cuda(), requires_grad=True)
-            else:
-                x_plus_noise = Variable(torch.from_numpy(
-                    x_plus_noise), requires_grad=True)
+            # if self.cuda:
+            #     x_plus_noise = Variable(torch.from_numpy(
+            #         x_plus_noise).cuda(), requires_grad=True)
+            # else:
+            #     x_plus_noise = Variable(torch.from_numpy(
+            #         x_plus_noise), requires_grad=True)
             output = self.pretrained_model(x_plus_noise)
 
             if index is None:
-                index = torch.argmax(output.data.cpu().numpy())
+                index = torch.argmax(output)
 
             one_hot = torch.zeros((1, output.size()[-1]), dtype=torch.float32)
             one_hot[0][index] = 1
-            if self.cuda:
-                one_hot = Variable(torch.from_numpy(
-                    one_hot).cuda(), requires_grad=True)
-            else:
-                one_hot = Variable(torch.from_numpy(
-                    one_hot), requires_grad=True)
+            # if self.cuda:
+            #     one_hot = Variable(torch.from_numpy(
+            #         one_hot).cuda(), requires_grad=True)
+            # else:
+            #     one_hot = Variable(torch.from_numpy(
+            #         one_hot), requires_grad=True)
             one_hot = torch.sum(one_hot * output)
 
             if x_plus_noise.grad is not None:
                 x_plus_noise.grad.data.zero_()
-            one_hot.backward(retain_variables=True)
+            # one_hot.backward(retain_variables=True)
+            torch.autograd.grad(one_hot, x_plus_noise, create_graph=True)
 
-            grad = x_plus_noise.grad.data.cpu().numpy()
+            grad = x_plus_noise.grad
 
             if self.magnitutde:
                 total_gradients += (grad * grad)

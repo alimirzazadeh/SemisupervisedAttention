54b261a1f7fbb590602746e3640d37f5a1fdac03
krinetic1234 <56781484+krinetic1234@users.noreply.github.com>
2021-07-27 00:14:35 -0700

    Updates

--- submit.sh
+++ submit.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #
-#BATCH --job-name=exp_test
+#BATCH --job-name=exp_krish25
 #
 #SBATCH --time=48:00:00
 #SBATCH --ntasks=1
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
 #SBATCH -C GPU_MEM:32GB
-#SBATCH -e /scratch/groups/rubin/krish05m/AttentionMap/saved_batches/exp_test.err
-#SBATCH -o /scratch/groups/rubin/krish05m/AttentionMap/saved_batches/exp_test.out
-export BATCH_DIRECTORY=exp_test
+#SBATCH -e /scratch/groups/rubin/krish05m/AttentionMap/saved_batches/exp_krish25.err
+#SBATCH -o /scratch/groups/rubin/krish05m/AttentionMap/saved_batches/exp_krish25.out
+export BATCH_DIRECTORY=exp_krish25
 export LEARNING_RATE=0.000001
 export NUM_EPOCHS=200
 export BATCH_SIZE=4
 export RESOLUTION_MATCH=4
-export SIMILARITY_METRIC=0
-export ALPHA=8
+export SIMILARITY_METRIC=2
+export ALPHA=32
 
 mkdir /scratch/groups/rubin/krish05m/AttentionMap/saved_batches/$BATCH_DIRECTORY
 ml python/3.9.0
